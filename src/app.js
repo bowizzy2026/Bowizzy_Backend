27c837eaae9a3feb4f1ed7179e5dc1cfae4f3a75
Himanshu <himanshu.d@wizzybox.com>
2026-04-01 14:15:30 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,15 @@ app.use((req, res, next) => {
 });
 
 // Middlewares
-app.use(cors({ origin: ["https://api.bowizzy.com", "http://localhost:5173", "https://bowizzy.com"] }));
+app.use(cors({
+  origin: ["https://bowizzy.com", "http://localhost:5173"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
+// Handle preflight requests explicitly
+app.options("*", cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
