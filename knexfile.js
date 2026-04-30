45a69e806c32795812301babc3cd7323ac5d1113
Himanshu <himanshu.d@wizzybox.com>
2026-04-30 12:35:45 +0530

    knex change for prod

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ const config = {
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      ssl: { rejectUnauthorized: false }
+      // ssl: { rejectUnauthorized: false }
     },
     migrations: {
       directory: "./migrations",
